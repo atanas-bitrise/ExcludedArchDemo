4b1e0f6348d2c80644919c4ad5e9accc4062a24a
Atanas Chanev <atanas.chanev@bitrise.io>
2023-06-26 10:57:55 +0100

    All targets with excluded architecture

--- Core/Package.swift
+++ Core/Package.swift
@@ -20,7 +20,10 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "Core",
-            dependencies: []),
+            dependencies: [],
+            swiftSettings: [
+                .define("EXCLUDED_ARCHS[sdk=iphonesimulator*] = arm64")
+            ]),
         .testTarget(
             name: "CoreTests",
             dependencies: ["Core"],
